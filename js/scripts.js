c81466db9ec290e398bf0ebc2141cc2dffb316fb
jwnardine <jwnardine@gmail.com>
2015-10-23 15:00:19 -0700

    write the first test

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var beerBottle = function(input) {
+  return input + " bottles of beer on the wall, " + input + " bottles of beer! We take one down, and pass it around"
+
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
-describe('input', function() {
-  it("is equal to the number inputted by the user", function() {
-    expect(input(99)).to.equal(99);
+describe('beerBottle', function() {
+  it("writes out '(input) bottles of beer on the wall, (input) bottles of beer! We take on down, and pass it around'", function() {
+    expect(beerBottle(99)).to.equal('99 bottles of beer on the wall, 99 bottles of beer! We take one down, and pass it around');
   });
 });
