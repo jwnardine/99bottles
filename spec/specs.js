ceda36ec8d64c43debe67d669ae5933a84051b04
jwnardine <jwnardine@gmail.com>
2015-10-23 23:09:59 -0700

    create tests for the beer bottles list starting at 99, ending at zero, and decreasing incrementally by zero

--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,17 @@
 describe('beerBottle', function() {
   it("is false for user inputs less than zero", function() {
-  expect(beerBottle(-7)).to.equal(false);
+    expect(beerBottle(-7)).to.equal(false);
   });
+  it("finishes at 0", function() {
+    expect(beerBottle(0)).to.equal(true);
+  });
+
+  it("begins at 99", function() {
+    expect(beerBottle(99)).to.equal(true);
+  });
+
+  it("decreases by one", function() {
+    expect(beerBottle(-1)).to.equal(true);
+  });
+
 });
-gitcommitchange
